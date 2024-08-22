0c7958d9e9edeed54649e691dcbd162014b8d492
NCatariMamani <128624201+NCatariMamani@users.noreply.github.com>
2024-08-21 22:13:04 -0400

    Fix_reservaciones

---
+++ prisma/migrations/20240821222427_update_reservaciones/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "reservaciones" ALTER COLUMN "costoHabitacion" SET DATA TYPE TEXT,
+ALTER COLUMN "costoExtra" SET DATA TYPE TEXT,
+ALTER COLUMN "total" SET DATA TYPE TEXT;
